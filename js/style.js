adf5a7e862493f12b79071a18b71081274eda23e
Sam Penner <60679136+Samwpenner@users.noreply.github.com>
2022-08-06 18:01:16 -0700

    DONE
    and done

---
+++ js/style.js
@@ -0,0 +1,13 @@
+ScrollReveal({ duration: 1000})
+
+ScrollReveal().reveal('#jumbo1', { delay: 500});
+
+ScrollReveal().reveal('#jumbo2', { delay: 1000});
+
+ScrollReveal().reveal('.ingredients', { delay: 500});
+
+ScrollReveal().reveal('.PERSON', { delay: 500});
+
+ScrollReveal().reveal('.REFILL', { delay: 500});
+
+ScrollReveal().reveal('#jumbo3', { delay: 500});
